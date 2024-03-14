9193d490436de2db51adafe4d7f04b050d95f813
Sebastián Centurión <sebastiancenturion27@gmail.com>
2024-03-14 02:12:26 +0000

    subo cambios hasta el momento

--- src/app.py
+++ src/app.py
@@ -1,4 +1,34 @@
-from utils import db_connect
-engine = db_connect()
 
-# your code here
+import sqlalchemy
+import os
+import psycopg2
+
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+# 
+# dbname = os.getenv("DB_NAME")
+# user = os.getenv("DB_USER")
+# password = os.getenv("DB_PASSWORD")
+# host = os.getenv("DB_HOST")
+# port = os.getenv("DB_PORT")
+
+# conn = psycopg2.connect(
+#     dbname=dbname,
+#     user=user,
+#     password=password,
+#     host=host,
+#     port=port
+# )
+
+load_dotenv()
+# 1) Connect to the database here using the SQLAlchemy's create_engine function
+
+connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+engine = create_engine(connection_string).execution_options(autocommit=True)
+try:
+    connection = engine.connect()
+    print("Connected to the database!")
+except Exception as e:
+    print(f"Error: {e}")
+    # createdb -h localhost -U gitpod estadisticasNBA
