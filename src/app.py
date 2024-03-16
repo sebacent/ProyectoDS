03c017807ec01eaef9aa84ecb0aca6792098cef4
josedrosales <124944389+josedrosales@users.noreply.github.com>
2024-03-16 03:08:02 +0000

    CREACION DATABASE Y TABLA

--- src/app.py
+++ src/app.py
@@ -2,35 +2,29 @@
 import sqlalchemy
 import os
 import psycopg2
-
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-# 
-# dbname = os.getenv("DB_NAME")
-# user = os.getenv("DB_USER")
-# password = os.getenv("DB_PASSWORD")
-# host = os.getenv("DB_HOST")
-# port = os.getenv("DB_PORT")
-
-# conn = psycopg2.connect(
-#     dbname=dbname,
-#     user=user,
-#     password=password,
-#     host=host,
-#     port=port
-# )
-
 load_dotenv()
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
 
-connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-engine = create_engine(connection_string).execution_options(autocommit=True)
+dbname = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+
 try:
-    connection = engine.connect()
-    print("Connected to the database!")
-except Exception as e:
-    print(f"Error: {e}")
-    # createdb -h localhost -U gitpod estadisticasNBA
-# psql -h 127.0.0.1 -p 5432 -U gitpod -W -d estadisticasNBA
-#psql -h 127.0.0.1 -p 5432 -U gitpod -W postgres -d estadisticasNBA
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password
+    )
+
+    # Crear un cursor para ejecutar consultas SQL
+    cursor = connection.cursor()
+
+    # Crear la base de datos si no existe
+    cursor.execute("CREATE DATABASE IF NOT EXISTS NBA;")
+    
+except psycopg2.Error as error:
+    print("Error al conectar o interactuar con la base de datos:", error)
